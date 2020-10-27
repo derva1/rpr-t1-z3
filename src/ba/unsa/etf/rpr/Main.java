d27141fe229c5b77c8acdeec332e08c60235d811
derva1 <vdervisevi1@etf.unsa.ba>
2020-10-27 17:18:47 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Sat s = new Sat(15,45,40);
+    s.Ispisi();
+    s.Sljedeci();
+    s.Ispisi();
+    s.Prethodni();
+    s.pomjeriZa(-48);
+    s.Ispisi();
+
+    }
+}
